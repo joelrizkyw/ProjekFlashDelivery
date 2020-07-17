4e62310e630454b87ecd00d9546cf0326678734c
joelrizkyw <57706694+joelrizkyw@users.noreply.github.com>
2020-07-17 12:30:18 +0700

    second commit

--- FlashDeliveryProject/app/src/main/java/com/example/flashdeliveryproject/Fragment/AboutFragment.java
+++ FlashDeliveryProject/app/src/main/java/com/example/flashdeliveryproject/Fragment/AboutFragment.java
@@ -1,23 +1,46 @@
 package com.example.flashdeliveryproject.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.flashdeliveryproject.MainActivity;
+import com.example.flashdeliveryproject.MapsActivity;
 import com.example.flashdeliveryproject.R;
 
 public class AboutFragment extends Fragment {
+
+    private Button btnLokasi;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnLokasi = view.findViewById(R.id.btnLokasi);
+
+        btnLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), MapsActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
---
+++ FlashDeliveryProject/app/src/main/java/com/example/flashdeliveryproject/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.flashdeliveryproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
